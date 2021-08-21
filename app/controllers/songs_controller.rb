f03488615fad5fcea91e237ce35416e36eb7fce7
DabooUA <sbardysh@tutanota.com>
2021-08-21 14:57:18 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,8 +50,5 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title)
   end
-
-  def artist_params
-    params.require(:artist).permit(:name)
-  end
+  
 end
